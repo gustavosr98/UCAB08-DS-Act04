9f0098d123bd96bf298e94a77654e93f1ad3787d
Javier Andrade <javierandrade2107@gmail.com>
2020-07-21 11:42:16 -0400

    Prueba con login

--- petromiles-frontend/test/e2e/jest/sell-points.e2e-spec.js
+++ petromiles-frontend/test/e2e/jest/sell-points.e2e-spec.js
@@ -16,16 +16,53 @@ describe("[E2E] As a client I want to exchange points so I can get its dollar eq
   });
 
   it("[Selenium] on Firefox", async () => {
-    await driver.get("http://localhost:8081/sell-points");
+    await driver.get("https://petromiles-frontend.herokuapp.com/");
     await driver
       .manage()
       .window()
-      .setRect(960, 1053);
+      .setRect(1382, 744);
+    await driver.findElement(By.id("input-27")).click();
+    await driver
+      .findElement(By.id("input-27"))
+      .sendKeys("jaandrade.17@est.ucab.edu.ve");
+    await driver.findElement(By.id("password")).click();
+    {
+      const element = await driver.findElement(By.css(".theme--dark"));
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element)
+        .perform();
+    }
+    await driver.findElement(By.id("password")).sendKeys("prueba1234");
+    await driver.findElement(By.css(".theme--dark")).click();
+    {
+      const element = await driver.findElement(By.CSS_SELECTOR, "body");
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element, 0, 0)
+        .perform();
+    }
+    {
+      const element = await driver.findElement(
+        By.css("div:nth-child(2) > .v-main .v-app-bar__nav-icon .v-icon")
+      );
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element)
+        .perform();
+    }
     await driver
       .findElement(
         By.css("div:nth-child(2) > .v-main .v-app-bar__nav-icon .v-icon")
       )
       .click();
+    {
+      const element = await driver.findElement(By.CSS_SELECTOR, "body");
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element, 0, 0)
+        .perform();
+    }
     await driver
       .findElement(
         By.css(
@@ -33,15 +70,57 @@ describe("[E2E] As a client I want to exchange points so I can get its dollar eq
         )
       )
       .click();
-    await driver.findElement(By.id("withdraw-points-input")).click();
-    await driver.findElement(By.id("withdraw-points-input")).sendKeys("5000");
+    await driver
+      .findElement(By.css(".v-overlay--active > .v-overlay__scrim"))
+      .click();
+    await driver.findElement(By.id("input-197")).click();
+    await driver.findElement(By.id("input-197")).sendKeys("2000");
     await driver.findElement(By.css(".v-select__selections")).click();
-    await driver.findElement(By.xpath("//body/div/div[2]/div/div")).click();
+    await driver.findElement(By.id("list-item-228-0")).click();
+    {
+      const element = await driver.findElement(By.css(".col .v-btn__content"));
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element)
+        .perform();
+    }
     await driver.findElement(By.css(".col .v-btn__content")).click();
+    {
+      const element = await driver.findElement(By.CSS_SELECTOR, "body");
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element, 0, 0)
+        .perform();
+    }
+    {
+      const element = await driver.findElement(
+        By.css(".success > .v-btn__content")
+      );
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element)
+        .perform();
+    }
     await driver.findElement(By.css(".success > .v-btn__content")).click();
+    {
+      const element = await driver.findElement(By.CSS_SELECTOR, "body");
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element, 0, 0)
+        .perform();
+    }
+    {
+      const element = await driver.findElement(
+        By.css(".v-btn--router > .v-btn__content")
+      );
+      await driver
+        .actions({ bridge: true })
+        .moveToElement(element)
+        .perform();
+    }
     await driver
       .findElement(By.css(".v-btn--router > .v-btn__content"))
       .click();
-    await driver.executeScript("window.scrollTo(0,0)");
+    await driver.close();
   });
 });
